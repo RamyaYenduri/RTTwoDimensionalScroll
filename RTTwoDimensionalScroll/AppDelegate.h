d24cbaa0e974730ca7cf8d34f633c87596663b3a
santhosh kumar <santhosh@riktamtech.com>
2016-08-30 11:49:38 +0530

    Initial Commit

---
+++ RTTwoDimensionalScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RTTwoDimensionalScroll
+//
+//  Created by Santhosh on 30/08/16.
+//  Copyright © 2016 riktam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RTTwoDimensionalScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RTTwoDimensionalScroll
+//
+//  Created by Santhosh on 30/08/16.
+//  Copyright © 2016 riktam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
